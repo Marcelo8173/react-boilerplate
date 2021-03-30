8b66f491937d446ed77878e1d2d1885b7847b2a4
Marcelo8173 <marceloandrebio@gmail.com>
2021-03-30 16:53:34 -0300

    install plop

--- src/components/First/index.tsx
+++ src/components/First/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import * as S from './styles'
 
-const First = ({ title = 'realize' }) => {
+const First = ({ title = 'realize b2c' }) => {
   return (
     <S.Wrapper>
       <h1>{title}</h1>
